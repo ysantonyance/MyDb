fd2747e4e945ea2afc5e44b3f2d3f25868a42916
ysantonyance <149514654+ysantonyance@users.noreply.github.com>
2026-01-28 14:15:11 +0200

    Add files via upload

---
+++ MyDb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyDatabase
+{
+    public class MyDb : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=localhost; " +
+                "Database=MyDb; " +
+                "Integrated Security=True; " +
+                "TrustServerCertificate=True;"
+                );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Identity.Client;
+using MyDatabase;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter login: ");
+        string login = Console.ReadLine();
+
+        Console.WriteLine("Enter password: ");
+        string password = Console.ReadLine();
+
+        var context = new MyDb();
+        context.Database.EnsureCreated();
+
+        var user = context.Users.FirstOrDefault(u => u.Username == login);
+
+        if (user != null)
+        {
+            if (user.Password == password)
+            {
+                Console.WriteLine("Welcome back!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect password!");
+            }
+        }
+        else
+        {
+            if (password.Length < 6)
+            {
+                Console.WriteLine("Password needs to include at least 6 symbols");
+                return;
+            }
+
+            if (!password.Any(char.IsDigit))
+            {
+                Console.WriteLine("Password needs to include at least 1 digit");
+                return;
+            }
+
+            context.Users.Add(new User
+            {
+                Username = login,
+                Password = password,
+                CreatedAt = DateTime.Now
+            });
+            context.SaveChanges();
+            Console.WriteLine("User is registrated");
+        }
+    }
+}
+
